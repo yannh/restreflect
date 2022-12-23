4c329918f6f53b74650c40079c784d0a08dc88fe
Yann Hamon <yann.hamon@contentful.com>
2022-12-23 12:50:46 +0100

    Add images

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,31 @@ use std::ffi::OsStr;
 struct Asset;
 
 
+fn file_mimetype(filename: &str, default: mime::Mime) -> mime::Mime {
+    let extension = Path::new(filename)
+        .extension()
+        .and_then(OsStr::to_str)
+        .map(|s| s.to_lowercase());
+
+    let mime_webp: mime::Mime = "image/webp".parse().unwrap();
+    match extension {
+        Some(ext) => match ext.as_str() {
+            "css" => mime::TEXT_CSS_UTF_8,
+            "gif" => mime::IMAGE_GIF,
+            "html" | "htm" => mime::TEXT_HTML_UTF_8,
+            "jpeg" | "jpg" => mime::IMAGE_JPEG,
+            "png" => mime::IMAGE_PNG,
+            "js" => mime::TEXT_JAVASCRIPT,
+            "svg" => mime::IMAGE_SVG,
+            "webp" => mime_webp, // webp not supported https://github.com/hyperium/mime/pull/129
+                                 // unfortunately the mime library is unmaintained
+            _ => default,
+        },
+        _ => default,
+    }
+}
+
+
 #[fastly::main]
 fn main(req: Request) -> Result<Response, Error> {
     let not_found = Ok(Response::from_status(StatusCode::NOT_FOUND)
@@ -41,6 +66,25 @@ fn main(req: Request) -> Result<Response, Error> {
         return Ok(Response::from_status(StatusCode::OK)
             .with_content_type(mime::TEXT_HTML_UTF_8)
             .with_body(resp.to_string()))
+    }
+
+    let images_paths = ["/image/jpeg", "/image/png", "/image/svg", "/image/webp"];
+    if images_paths.contains(&req.get_path()) {
+        let img_path = match req.get_path() {
+            "/image/jpeg" => "jpeg.jpeg",
+            "/image/png" => "png.png",
+            "/image/svg" => "svg.svg",
+            "/image/webp" => "webp.webp",
+            _ => ""
+        };
+
+        return match Asset::get(img_path) {
+            Some(asset) => Ok(Response::from_status(StatusCode::OK)
+                .with_body_octet_stream(asset.data.as_ref())
+                .with_content_type(file_mimetype(img_path, mime::APPLICATION_OCTET_STREAM))),
+
+            None => not_found,
+        }
     }
 
     if req.get_path() == "/user-agent" {
