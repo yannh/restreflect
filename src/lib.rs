bde9325b8d8cd35c773eaf89c8bd748aaa0a239d
Yann Hamon <yann.hamon@contentful.com>
2022-12-25 11:16:50 +0100

    reflect request query parameters

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,12 @@ pub fn req_to_json(req: Request) -> String {
         .map(|m| (m.0.as_str(), m.1.to_str().unwrap_or("")))
         .collect();
 
+    let arg_pairs: Vec<(String, String)> = req.get_query().unwrap();
+    let args: HashMap<&str, &str> = arg_pairs.iter().map(|m| (m.0.as_str(), m.1.as_str()))
+        .collect();
+
     let resp = json!({
+            "args": args,
             "headers": headers,
             "origin": req.get_client_ip_addr(),
             "url": req.get_url_str()
