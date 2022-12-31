3a0d24c843a65738e68d2b9ba3bcc7f564440f1d
Yann Hamon <yann.hamon@contentful.com>
2022-12-31 17:15:14 +0100

    GET /etag

--- src/response_inspection.rs
+++ src/response_inspection.rs
@@ -1,4 +1,5 @@
-use fastly::http::{StatusCode};
+use fastly::http::{HeaderValue, StatusCode};
+use regex::Regex;
 use fastly::{Error, mime, Request, Response};
 use std::collections::HashMap;
 
@@ -44,4 +45,57 @@ pub fn response_headers_get(req: &Request) -> Result<Response, Error> {
 /// Returns a set of response headers from the query string
 pub fn response_headers_post(req: &Request) -> Result<Response, Error> {
     return response_headers_get(req)
+}
+
+#[utoipa::path(
+    get,
+    path = "/etag",
+    tag = "Response inspection",
+    params(
+        ("if-none-match" = String, Header),
+        ("if-match" = String, Header),
+    ),
+    responses(
+        (status = 200, description = "Response headers", content_type = "application/json"),
+        (status = 412, description = "match", content_type = "application/json")
+    )
+)]
+/// Assumes the resource has the given etag and responds to If-None-Match and If-Match headers appropriately.
+pub fn etag(req: &Request) -> Result<Response, Error> {
+    let caps = Regex::new(r"/etag/(\w+)$").unwrap()
+        .captures(req.get_path());
+    if caps.is_some() {
+        let etag = caps.unwrap().get(1).map_or("404", |m| m.as_str());
+        let d = HeaderValue::from_static("");
+        let if_none_match: Vec<&str> = req
+            .get_header("if-none-match")
+            .unwrap_or(&d)
+            .to_str()
+            .unwrap_or("")
+            .split(",")
+            .collect();
+        if if_none_match.contains(&etag) || if_none_match.contains(&"*"){
+            return Ok(Response::from_status(StatusCode::NOT_MODIFIED)
+                .with_content_type(mime::APPLICATION_JSON));
+        }
+
+        let if_match: Vec<&str> = req
+            .get_header("if-none-match")
+            .unwrap_or(&d)
+            .to_str()
+            .unwrap_or("")
+            .split(",")
+            .collect();
+        if !if_match.contains(&etag) && !if_match.contains(&"*"){
+            return Ok(Response::from_status(StatusCode::PRECONDITION_FAILED)
+                .with_content_type(mime::APPLICATION_JSON));
+        }
+
+        return Ok(Response::from_status(StatusCode::OK)
+            .with_header("ETag", etag)
+            .with_content_type(mime::APPLICATION_JSON));
+    }
+
+    return Ok(Response::from_status(StatusCode::NOT_FOUND)
+        .with_content_type(mime::APPLICATION_JSON));
 }
